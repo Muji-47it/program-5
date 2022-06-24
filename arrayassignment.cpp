14e8d976d0be850670e1142c03e949f34565fdad
Muji-47it <107984150+Muji-47it@users.noreply.github.com>
2022-06-24 16:33:46 +0500

    Update arrayassignment.cpp

--- arrayassignment.cpp
+++ arrayassignment.cpp
@@ -1,14 +1,14 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main()
-{
-    int arr[]={1,2,3,4,5};
+int main() {
+    // Write C++ code here
+    int arr[]={32,4,2,3,5};
     int min;
     min=arr[0];
     for (int i = 0; i < 4; i++)
     {
         if(arr[i]<min)
-        min=arr[i];
+      min=arr[i];
     }
     cout<<min;   
 }
